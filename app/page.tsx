1451f536b5b1d83c81370fc3d47f98d402de3e70
Craig <craigthomashill@gmail.com>
2023-10-21 03:27:57 +0100

    shemmy tings around again

---
+++ app/page.tsx
@@ -0,0 +1,19 @@
+// app/lib/GameContext.tsx
+
+"use client";
+
+import React from "react";
+import { GameProvider } from "./lib/GameContext";
+import CityView from "./components/CityView";
+
+const Page: React.FC = () => {
+  return (
+    <GameProvider>
+      <div className="game-container">
+        <CityView />
+      </div>
+    </GameProvider>
+  );
+};
+
+export default Page;
